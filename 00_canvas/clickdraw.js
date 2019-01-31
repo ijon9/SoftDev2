3d773da1b4864c143f6bd89d07ce60803fc3fbec
unknown <ijon@stuy.edu>
2019-01-30 22:22:31 -0500

    hw1

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,9 +1,26 @@
-var button = document.getElementById("b");
-button.addEventListener('click', drawRect);
+const c = document.getElementById("slate");
+const ctx = c.getContext("2d");
 
 var drawRect = function(e) {
-  var canvas = document.getElementById("slate");
-  var ctx = c.getContext("2d");
   ctx.fillStyle = '#0000ff';
-  ctx.fillRect(10, 10, 150, 100);
+  ctx.fillRect(50, 50, 150, 100);
+};
+
+var drawDot = function(e) {
+  ctx.beginPath();
+  ctx.ellipse(100, 100, 50, 75, Math.PI / 4, 0, 2 * Math.PI);
+  ctx.stroke();
 }
+
+var clear = function(e) {
+  ctx.clearRect(0, 0, c.width, c.height);
+}
+
+var button = document.getElementById("rect");
+button.addEventListener('click', drawRect);
+
+var button = document.getElementById("clear");
+button.addEventListener('click', clear);
+
+var button = document.getElementById("dot");
+button.addEventListener('click', drawDot);
