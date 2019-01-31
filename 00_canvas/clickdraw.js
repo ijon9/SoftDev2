3d9236c44eafc0246e23f21307f6bf68c25d98b9
unknown <ijon@stuy.edu>
2019-01-31 06:55:15 -0500

    hw0

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,26 +1,54 @@
 const c = document.getElementById("slate");
 const ctx = c.getContext("2d");
+var drawState = false //false is box, true is dot
 
-var drawRect = function(e) {
-  ctx.fillStyle = '#0000ff';
-  ctx.fillRect(50, 50, 150, 100);
+
+var toggleState = function(e) {
+    if(drawState == false) {
+      drawState = true;
+      console.log(drawState)
+    }
+    else {
+      drawState = false;
+      console.log(drawState)
+    };
+    if (drawState == false) {
+      msg.innerHTML = "Current State: BOX"
+    }
+    else {
+      msg.innerHTML = "Current State: DOT"
+    }
 };
 
-var drawDot = function(e) {
-  ctx.beginPath();
-  ctx.ellipse(100, 100, 50, 75, Math.PI / 4, 0, 2 * Math.PI);
-  ctx.stroke();
+var getMousePos = function(evt) {
+    var rect = c.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
 }
 
+var drawShape = function(evt) {
+  pos = getMousePos(evt)
+  if (drawState == false) {
+    ctx.fillStyle = '#0000ff';
+    ctx.fillRect(pos.x, pos.y, 20, 20);
+  }
+  if (drawState == true) {
+    ctx.beginPath();
+    ctx.ellipse(pos.x, pos.y, 12, 12, Math.PI / 4, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = '#0000ff';
+    ctx.fill()
+  }
+}
 var clear = function(e) {
   ctx.clearRect(0, 0, c.width, c.height);
 }
 
-var button = document.getElementById("rect");
-button.addEventListener('click', drawRect);
+var t = document.getElementById("toggle");
+t.addEventListener('click', toggleState)
 
-var button = document.getElementById("clear");
-button.addEventListener('click', clear);
 
-var button = document.getElementById("dot");
-button.addEventListener('click', drawDot);
+var cls = document.getElementById("clear");
+cls.addEventListener('click', clear);
