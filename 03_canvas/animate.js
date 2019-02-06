db5bf40a3499fa1c9004bf1f573554ffbb8089a8
unknown <ijon@stuy.edu>
2019-02-05 21:51:33 -0500

    hw3

--- 03_canvas/animate.js
+++ 03_canvas/animate.js
@@ -1,45 +1,56 @@
-// smallChungus:  Robin Han & Isaac Jon
-// SoftDev2 PD7
-// K#01 -- ...and I want to Paint It Btter
-// 2019-01-31
+//Team MS Paint: Isaac Jon Imad Belkebir
+//SoftDev pd7
+//K #03: They lock us in the tower whenever we get caught
+//2/4/19
 
-//initially allows user to draw rectangles when mouse is pressed on canvas.
-var c = document.getElementById("playground");
-var ctx = document.getContext("2d")
-
-var goButton = document.getElementById("circle");
-//event listener for stopping animation
-var stopButton = document.getElementById("stop");
-
-goButton.addEventListener("click", animate);
-stopButton.addEventListener("click", stop);
-
-var requestID;
+var canvas = document.getElementById('playground');
+var start = document.getElementById('start');
+var stop = document.getElementById('stop');
+var ctx = canvas.getContext('2d');
+var animating = false;
+var growing = true;
 var radius = 0;
-var growing = false;
+var id;
 
-var clear = function(e) {
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-};
+start.addEventListener('click', function() {
+  if(!animating){
+    animating = true;
+    window.requestAnimationFrame(paint);
+  }
+});
+stop.addEventListener('click', function() {
+  animating = false;
+  window.cancelAnimationFrame(id);
+});
 
-var drawDot = function() {
-	clear()
-	if(radius == 0) {
-		growing = true;
-		radius++
-	}
-	if (radius >= 250) {
-		growing = false;
-	}
-	//draw the dot
-	ctx.beginPath();
-	ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
-	ctx.stroke();
-	ctx.fill();
-	...
-}
-
-var stopIt = function() {
-	console.log(requestID);
-	...
+function paint() {
+  //console.log(radius);
+  if(animating){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    if(radius > 0){
+    	ctx.beginPath();
+    	ctx.fillStyle = "#18f0f0";
+    	ctx.ellipse(300, 300, radius, radius, 0, 0, 2 * Math.PI);
+    	ctx.fill()
+    	ctx.stroke();
+    }
+      if(radius == 0){
+        radius++;
+        growing = true;
+      }
+      if(radius >= 300){
+        growing = false;
+      }
+      if(radius <= 0){
+        growing = true;
+      }
+      if(growing){
+        radius += 5;
+      }else{
+        radius -= 5;
+      }
+      window.requestAnimationFrame(paint);
+    }else{
+      window.cancelAnimationFrame(id);
+    }
 };
