cfb121bbe5849c649825f7a5459d377de1072cdb
Isaac Jon <ijon@stuy.edu>
2019-04-15 07:48:06 -0400

    Update listComp.py

--- 17_listcomp/listComp.py
+++ 17_listcomp/listComp.py
@@ -1,3 +1,7 @@
+#Team Bob: Issan Jon and Jerry Ye
+# SoftDev2 pd7
+# K17 -- PPFTLCW
+# 2019-04-15 M
 #1)
 l12 = [str(x) * 2 for x in range(0,9,2)]
 print(l12)
@@ -24,7 +28,6 @@ l32 = [x*y for x in range(0,3) for y in range(0,3)]
 print(l32)
 #4) + 5)
 
-
 primes = [2]
 for x in range(3, 101, 2):
     for k in primes:
@@ -39,4 +42,35 @@ for x in range(101):
 print(primes)
 print(composites)
 
-primes = 
+l = [2,3,5,7]
+#composites = [x * c if x * c <= 100 else 0 for x in l for c in range(2,50)]
+composites1 = [x*c for x in l for c in range(2,50) if x * c <= 100]
+composites1 = list(set(composites1))
+print(composites1)
+primes1 = [x for x in range(2,50) if not(x in composites1)]
+print(primes1)
+
+def getDivisors(num):
+    l = []
+    for x in range(1,num + 1):
+        if num % x == 0:
+            l.append(x)
+    return l
+def getDivisors2(num):
+    return [x for x in range(1,num+1) if num %x == 0 ]
+print(getDivisors(10))
+print(getDivisors2(10))
+
+def transpose(matrix):
+    l = []
+    for r in range(len(matrix[0])):
+        l2 = []
+        for c in range(len(matrix)):
+            l2.append(matrix[c][r])
+        l.append(l2)
+    return l
+print(transpose([[1,2,3],[1,2,3]]))
+
+def transpose2(matrix):
+    return [[matrix[r][c] for r in range(len(matrix))] for c in range(len(matrix[0]))]
+print(transpose2([[1,2,3],[1,2,3]]))
